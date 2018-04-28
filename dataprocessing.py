23df0895cb529b12bfdf66ce7dcd31d903709c82
siyuanchai1999 <31422075+siyuanchai1999@users.noreply.github.com>
2018-04-27 20:43:15 -0500

    pytho program updated

--- dataprocessing.py
+++ dataprocessing.py
@@ -1,19 +1,35 @@
 import serial
 import csv
+import numpy as np
+import pandas as pd
+import time
 ser = serial.Serial('/dev/ttyACM0',9600)
  
 
-listA_x = []
-listA_y = []
+
+listA_x = [0]
+listA_y = [0]
+listAcc_E =[0]
+listAcc_N =[0]
 listG = []
 listH = []
+listV_E = [0]
+listV_N = [0]
+listD_E = [1]
+listD_N = [0]
 
 sumA_x = 0.0
 sumA_y = 0.0
 sumG = 0.0
 sumH = 0.0
- 
-def process_imu_data(line): 
+sumD_x = 0.0
+sumD_y = 0.0
+index = 1
+time.sleep(6)
+ser.write('!'.encode)
+
+def process_imu_data(line):
+    delta_t = 0.1
     str_line = line.decode()
     if(str_line[0] == 'A'):
         comma_pos = str_line.index(',')
@@ -25,6 +41,7 @@ def process_imu_data(line):
         sumA_y = sumA_y + A_y
         listA_x.append(A_x)
         listA_y.append(A_x)
+        
     elif(str_line[0] == 'G'):
         G = float(str_line[1:-2])
         global sumG
@@ -33,27 +50,54 @@ def process_imu_data(line):
     elif(str_line[0] == 'H'):
         H = float(str_line[1:-2])
         global sumH
+        global index
         sumH = sumH + H
+        H = H - 90
         listH.append(H)
+        accele_E = listA_y[index] *np.cos(H) - listA_x[index]* np.sin(H) #convet IMU coordinates to world E-N coordinate
+        accele_N = listA_y[index] *np.sin(H) + listA_x[index]* np.cos(H)
+        listAcc_E.append(accele_E)
+        listAcc_N.append(accele_N)
+        
+        vel_E = listV_E[index - 1] + (accele_E + listAcc_E[index-1])/2 * 9.8 *delta_t #update velocity
+        vel_N = listV_N[index - 1] + (accele_N + listAcc_N[index-1])/2 * 9.8 *delta_t
+        listV_E.append(vel_E)
+        listV_N.append(vel_N)
+
+        dis_E = listD_E[index - 1] + (vel_E+listV_E[index-1])/2 * delta_t  #update displacement
+        dis_N = listD_N[index - 1] + (vel_N+listV_N[index-1])/2 * delta_t
+        listD_E.append(dis_E)
+        listD_N.append(dis_N)
+        
+        global index = index + 1
     elif(str_line[0] == 'T'):
         if(int(str_line[1:-2]) == 70):
             with open('data.csv','w') as mycsv:
                 wr = csv.writer(mycsv, quoting = csv.QUOTE_ALL)
-                listA_x.append(sumA_x)
+                #listA_x.append(sumA_x)
                 wr.writerow(listA_x)
-                listA_x.pop()
+                #listA_x.pop()
                  
-                listA_y.append(sumA_y)
+                #listA_y.append(sumA_y)
                 wr.writerow(listA_y)
-                listA_y.pop()
+                #listA_y.pop()
+
+                wr.writerow(listAcc_E)
+                wr.writerow(listAcc_N)
+
+                wr.writerow(listV_E)
+                wr.writerow(listV_N)
+
+                wr.writerow(listD_E)
+                wr.writerow(listD_E)
                 
-                listG.append(sumG)
+                #listG.append(sumG)
                 wr.writerow(listG)
-                listG.pop()
+                #listG.pop()
                 
-                listH.append(sumH)
+                #listH.append(sumH)
                 wr.writerow(listH)
-                listH.pop()
+                #listH.pop()
             print("done output")
     else:
         print("err message")
---
+++ runningArduino/runningArduino.ino
@@ -0,0 +1,323 @@
+#include <Servo.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SparkFunLSM9DS1.h>
+
+Servo esc,steering;
+LSM9DS1 imu;
+
+#define LSM9DS1_M  0x1E // Would be 0x1C if SDO_M is LOW
+#define LSM9DS1_AG  0x6B // Would be 0x6A if SDO_AG is LOW
+
+#define PRINT_CALCULATED
+//#define PRINT_RAW
+#define PRINT_SPEED 100 // 100 ms between prints
+
+#define DECLINATION 1.29 // Declination (degrees) in St. Louis,MO
+float recentFive[5];
+float mxFive[5];
+float myFive[5];
+long index = 0;
+float headingMean =0;
+float mxMean = 0;
+float myMean = 0;
+int numAverage = 5;
+
+
+unsigned long lastPrint = 0; // Keep track of print time
+unsigned long threshold;
+
+
+int throttlePin = 6;
+int steerPin = 5;
+int motorPin = 9;
+int servoPin = 11;
+int Ledpin = 13;
+boolean readyNot = 0;
+int throttle;
+int steer;
+
+int delta = 100;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  imu.settings.device.commInterface = IMU_MODE_I2C;  //imu setting 
+  imu.settings.device.mAddress = LSM9DS1_M;
+  imu.settings.device.agAddress = LSM9DS1_AG;
+  
+  Serial.begin(9600);
+ 
+  esc.attach(motorPin); // pin assignment for motor & servo
+  steering.attach(servoPin);
+  esc.write(90);
+  delay(4000);
+  pinMode(throttlePin, INPUT);
+  pinMode(steerPin, INPUT);
+  pinMode(Ledpin, OUTPUT);
+  if (!imu.begin())
+  {
+    Serial.println("Failed to communicate with LSM9DS1.");
+    Serial.println("Double-check wiring.");
+    Serial.println("Default settings in this sketch will " \
+                  "work for an out of the box LSM9DS1 " \
+                  "Breakout, but may need to be modified " \
+                  "if the board jumpers are.");
+    while (1)
+      ;
+  }
+  threshold = millis();
+  digitalWrite(Ledpin, HIGH);
+  delay(1000);
+  digitalWrite(Ledpin, LOW);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available()){
+    char in = Serial.read();
+    if(in == '!'){
+      readyNot = 1;
+      digitalWrite(Ledpin, HIGH);
+    }
+  }
+  
+  if(readyNot){
+    if(millis()>threshold){
+      throttle = pulseIn(throttlePin, HIGH);
+      steer = pulseIn(steerPin, HIGH);
+      //Serial.print("Before:    ");
+      //Serial.print("throttle:");
+      //Serial.println(throttle);
+      //Serial.print(" steer:");
+      //Serial.println(steer);*/
+      throttle = get_throttle(throttle);
+      steer = get_steer(steer);
+      /*Serial.print("after:    ");
+      Serial.print("throttle:");
+      Serial.println(throttle);
+      Serial.print(" steer:");
+      Serial.println(steer);*/
+      Serial.print('T');
+      Serial.println(throttle);
+      if(throttle == 70){
+        readyNot = 0;
+        digitalWrite(Ledpin,LOW);
+      }
+      //Serial.print('S');
+      //Serial.println(steer);
+      if(throttle == 70) throttle = 90;
+      esc.write(throttle);
+      steering.write(steer);
+      threshold = millis() + delta;
+    }
+    imu_read();
+    imu_write();
+  }
+  
+}
+
+void imu_read(){
+  if ( imu.gyroAvailable() )
+  {
+    // To read from the gyroscope,  first call the
+    // readGyro() function. When it exits, it'll update the
+    // gx, gy, and gz variables with the most current data.
+    imu.readGyro();
+  }
+  if ( imu.accelAvailable() )
+  {
+    // To read from the accelerometer, first call the
+    // readAccel() function. When it exits, it'll update the
+    // ax, ay, and az variables with the most current data.
+    imu.readAccel();
+  }
+  if ( imu.magAvailable() )
+  {
+    // To read from the magnetometer, first call the
+    // readMag() function. When it exits, it'll update the
+    // mx, my, and mz variables with the most current data.
+    imu.readMag();
+  }
+}
+
+void imu_write(){
+  if ((lastPrint + PRINT_SPEED) < millis()){
+    //printGyro();  // Print "G: gx, gy, gz"
+    printAccel(); // Print "A: ax, ay, az"
+    //printMag();   // Print "M: mx, my, mz"
+    // Print the heading and orientation for fun!
+    // Call print attitude. The LSM9DS1's mag x and y
+    // axes are opposite to the accelerometer, so my, mx are
+    // substituted for each other.
+    printAttitude(imu.ax, imu.ay, imu.az, 
+                 -imu.my, -imu.mx, imu.mz);
+    Serial.println();
+    
+    lastPrint = millis(); // Update lastPrint time
+  }
+}
+
+void printGyro()
+{
+  // Now we can use the gx, gy, and gz variables as we please.
+  // Either print them as raw ADC values, or calculated in DPS.
+  Serial.print('G');
+#ifdef PRINT_CALCULATED
+  // If you want to print calculated values, you can use the
+  // calcGyro helper function to convert a raw ADC value to
+  // DPS. Give the function the value that you want to convert.
+  /*Serial.print(imu.calcGyro(imu.gx), 2);
+  Serial.print(", ");
+  Serial.print(imu.calcGyro(imu.gy), 2);
+  Serial.print(", ");*/
+  Serial.println(imu.calcGyro(imu.gz), 2);
+  //Serial.println(" deg/s");
+#elif defined PRINT_RAW
+  Serial.print(imu.gx);
+  Serial.print(", ");
+  Serial.print(imu.gy);
+  Serial.print(", ");
+  Serial.println(imu.gz);
+#endif
+}
+
+void printAccel()
+{  
+  // Now we can use the ax, ay, and az variables as we please.
+  // Either print them as raw ADC values, or calculated in g's.
+  Serial.print('A');
+#ifdef PRINT_CALCULATED
+  // If you want to print calculated values, you can use the
+  // calcAccel helper function to convert a raw ADC value to
+  // g's. Give the function the value that you want to convert.
+  Serial.print(imu.calcAccel(imu.ax), 2);
+  Serial.print(", ");
+  Serial.println(imu.calcAccel(imu.ay), 2);
+  //Serial.print(", ");
+  //Serial.print(imu.calcAccel(imu.az), 2);
+  //Serial.println(" g");
+#elif defined PRINT_RAW 
+  Serial.print(imu.ax);
+  Serial.print(", ");
+  Serial.print(imu.ay);
+  Serial.print(", ");
+  Serial.println(imu.az);
+#endif
+}
+
+
+float average(float arr[], float newValue, float preAverage){
+  if(index<numAverage){
+    float sum = 0;
+    arr[index] = newValue;
+    for(int i=0; i<=index;i++){
+      sum = sum + arr[i];
+    }
+    return sum/(index+1);
+  }else{
+    float oldValue = arr[index%numAverage];
+    arr[index%numAverage] = newValue;
+    return preAverage + (newValue - oldValue)/numAverage;
+  }
+}
+
+float headingCalc(float x, float y){
+  //Serial.print(x);
+  //Serial.print( "  ");
+  //Serial.println(y);
+  float h = 0.0;
+   if(y ==0){
+      if(x<0){
+        h = 180.0;
+       }else{
+        h = 0.0;
+    }
+  }else{
+    if(y>0){
+      h = 90 - atan(x/y)*180/PI;
+      //Serial.println("case 1");
+    }else if(y<0){
+      h = 270 - atan(x/y)*180/PI;
+      //Serial.println("case 2");
+    }
+  }
+  return h;
+}
+
+float manualHeadOffset(float h){
+  if( (0<=h && h<80) || (350<h && h<=360)){
+    h = h +10;
+    if(h>360) h = h - 360;
+  }else if( h>270 && h<=350){
+    h = 9/8*h - 270/8;
+  }else if(h >=80 && h<180){
+    h = 9/10*h +18;
+  }
+  return h;
+}
+
+
+// Heading calculations taken from this app note:
+// http://www51.honeywell.com/aero/common/documents/myaerospacecatalog-documents/Defense_Brochures-documents/Magnetic__Literature_Application_notes-documents/AN203_Compass_Heading_Using_Magnetometers.pdf
+//mx has maximum about 2400 at east direction
+//mx has minimum about 10 @ west direction
+//mx amp = 1195; offset mx by -1205
+
+//my has maximum about -3470 @ north direction
+//my has minimum about -5800 @ 172 degrees S
+// my amp = 1165; offset my by +4635
+
+
+void printAttitude(float ax, float ay, float az, float mx, float my, float mz)
+{
+  
+  float heading;
+  float mxOffset, myOffset;
+  mxMean = average(mxFive, mx, mxMean);
+  myMean = average(myFive, my, myMean);
+  /*Serial.print("mx: ");
+  Serial.print(mx);
+  Serial.print(" mxMean ");
+  Serial.print(mxMean);
+  Serial.print(" my: ");
+  Serial.print(my);
+  Serial.print(" myMean ");
+  Serial.println(myMean); */
+  mxOffset = mxMean - 1205;
+  myOffset = mxMean + 4635;
+  
+  heading = headingCalc(mxOffset, myOffset);
+  headingMean = average(recentFive, heading, headingMean);
+  index++;
+ // Serial.print("index: "); Serial.print(index);
+  Serial.print('H'); Serial.print(headingMean, 3);
+  //Serial.print("  average: "); Serial.println(headingMean,4);
+}
+
+int get_throttle(int t){
+  if((t<1490 && t>1440) || t ==0){
+    return 90;
+  }else{
+    if(t> 1490){
+      return 110;
+    }else{
+      return 70;
+    }
+  }
+}
+
+int get_steer(int s){
+  //s<1490 && s>1450)
+  //83 是中间
+  //55 最左
+  //111 最右
+  if((s<1500 && s>1440)|| s==0){
+    return 83;
+  }else{
+    return map(s,970,2021,55,111);
+  }
+}
+
