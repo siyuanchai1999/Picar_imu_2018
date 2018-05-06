7d46524a80907a026b602378d1a7e01ef0038b5a
siyuanchai1999 <31422075+siyuanchai1999@users.noreply.github.com>
2018-05-06 22:50:02 +0000

    Add files via upload

--- dataprocessing.py
+++ dataprocessing.py
@@ -5,10 +5,12 @@ import RPi.GPIO as GPIO
 #import pandas as pd
 import time
 import os
-ser = serial.Serial('/dev/ttyACM0',9600)
+from subprocess import call
+ser = serial.Serial('/dev/ttyACM0',115200)
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-
+GPIO.setup(18,GPIO.OUT)    
+GPIO.output(18,GPIO.LOW)
 
 listA_x = [0]
 listA_y = [0]
@@ -17,8 +19,8 @@ listAcc_N =[0]
 listG = []
 listH = [0]
 listV_E = [0]
-listV_N = [0]
-listD_E = [1]
+listV_N = [2.23]
+listD_E = [-1]
 listD_N = [0]
  
 sumA_x = 0.0
@@ -33,7 +35,7 @@ time.sleep(6)
 ser.write('!'.encode())
 
 def process_imu_data(line):
-    delta_t = 0.1
+    delta_t = 0.05
     str_line = line.decode()
     if(str_line[0] == 'A'):
         comma_pos = str_line.index(',')
@@ -43,8 +45,8 @@ def process_imu_data(line):
         global sumA_y
         sumA_x = sumA_x + A_x
         sumA_y = sumA_y + A_y
-        listA_x.append(A_x - 0.0742)
-        listA_y.append(A_y + 0.0653)
+        listA_x.append(A_x)
+        listA_y.append(A_y)
          
     elif(str_line[0] == 'G'):
         G = float(str_line[1:-2])
@@ -64,8 +66,8 @@ def process_imu_data(line):
         listAcc_E.append(accele_E)
         listAcc_N.append(accele_N)
           
-        vel_E = listV_E[index - 1] + (accele_E + listAcc_E[index-1])/2 * 9.8 *delta_t #update velocity
-        vel_N = listV_N[index - 1] + (accele_N + listAcc_N[index-1])/2 * 9.8 *delta_t
+        vel_E = listV_E[index - 1] + (accele_E + listAcc_E[index-1])/2 * 9.814 *delta_t #update velocity
+        vel_N = listV_N[index - 1] + (accele_N + listAcc_N[index-1])/2 * 9.814 *delta_t
         listV_E.append(vel_E)
         listV_N.append(vel_N)
 
@@ -81,34 +83,41 @@ def process_imu_data(line):
             while(os.path.isfile(outputName)):
                 dotPos = outputName.index('.')
                 outputName = outputName.replace(outputName[4:dotPos],str(int(outputName[4:dotPos]) +1))
+            try:
+                with open(outputName,'w') as mycsv:
+                    wr = csv.writer(mycsv, quoting = csv.QUOTE_ALL)
+                    #listA_x.append(sumA_x)
+                    wr.writerow(listA_x)
+                    #listA_x.pop()
+                     
+                    #listA_y.append(sumA_y)
+                    wr.writerow(listA_y)
+                    #listA_y.pop()
 
-            with open(outputName,'w') as mycsv:
-                wr = csv.writer(mycsv, quoting = csv.QUOTE_ALL)
-                #listA_x.append(sumA_x)
-                wr.writerow(listA_x)
-                #listA_x.pop()
-                 
-                #listA_y.append(sumA_y)
-                wr.writerow(listA_y)
-                #listA_y.pop()
+                    wr.writerow(listAcc_E)
+                    wr.writerow(listAcc_N)
 
-                wr.writerow(listAcc_E)
-                wr.writerow(listAcc_N)
+                    wr.writerow(listV_E)
+                    wr.writerow(listV_N)
 
-                wr.writerow(listV_E)
-                wr.writerow(listV_N)
-
-                wr.writerow(listD_E)
-                wr.writerow(listD_N)
-                
-                #listG.append(sumG)
-                wr.writerow(listG)
-                #listG.pop()
-                
-                #listH.append(sumH)
-                wr.writerow(listH)
-                #listH.pop()
-            print("done output")
+                    wr.writerow(listD_E)
+                    wr.writerow(listD_N)
+                    
+                    #listG.append(sumG)
+                    wr.writerow(listG)
+                    #listG.pop()
+                    
+                    #listH.append(sumH)
+                    wr.writerow(listH)
+                    #listH.pop()
+                    mycsv.flush()
+                    print("done output")
+            except Exception as e:
+                outputName = outputName.replace('data','error')
+                outputName = outputName.replace('.csv','.txt')
+                with open(outputName, 'w') as mylog:
+                    log.write('ERROR LOG\n')
+                    log.write(e)
             return 0
     else:
         print("err message")
@@ -123,7 +132,24 @@ while (con == 1):
             print(line)
             con = process_imu_data(line)
             
-        
 GPIO.output(18,GPIO.HIGH)
-print("program finished")
+time.sleep(1)
+GPIO.output(18,GPIO.LOW)
+time.sleep(1)
+GPIO.output(18,GPIO.HIGH)
+time.sleep(1)
+GPIO.output(18,GPIO.LOW)
+time.sleep(1)
+GPIO.output(18,GPIO.HIGH)
+time.sleep(1)
+GPIO.output(18,GPIO.LOW)
+time.sleep(1)
+GPIO.output(18,GPIO.HIGH)
+time.sleep(1)
+GPIO.output(18,GPIO.LOW)
+time.sleep(1)
+GPIO.output(18,GPIO.HIGH)
+time.sleep(6)
+call("sudo shutdown -h now", shell= True)
+
  
