ff816479807606476f6965c545d574bfcce046d1
siyuanchai1999 <31422075+siyuanchai1999@users.noreply.github.com>
2018-05-06 17:52:50 -0500

    program protype for csv output module

--- testOut.py
+++ testOut.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import csv
 import os
 import time
@@ -22,29 +21,3 @@ with open(outputName,'w') as mycsv:
     wr.writerow(listA)
 
 print(time.time()-start)
-=======
-import csv
-import os
-import time
-import numpy as np
-import pandas as pd
-outputName = 'data0.csv'
-start = time.time()
-arr = np.array(list(range(1,1000000)))
-listA = list(range(1,1000000))
-while(os.path.isfile(outputName)):
-    print("while")
-    dotPos = outputName.index('.')
-    outputName = outputName.replace(outputName[4:dotPos], str(int(outputName[4:dotPos])+1))
-with open(outputName,'w') as mycsv:
-    wr = csv.writer(mycsv, quoting = csv.QUOTE_ALL)
-    wr.writerow(listA)
-    wr.writerow(listA)
-    wr.writerow(listA)
-    wr.writerow(listA)
-    wr.writerow(listA)
-    wr.writerow(listA)
-    wr.writerow(listA)
-
-print(start-time.time())
->>>>>>> e769ca10bf42efe864e87ed328c96a54adb57e5c
